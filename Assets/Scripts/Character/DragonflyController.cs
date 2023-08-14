385ef5c72c65f2042b125086a4b537bb93a28d65
JeDa187 <j.saarela@dr.com>
2023-08-14 13:38:55 +0300

    Hahmon Luominen ja Lento

---
+++ Assets/Scripts/Character/DragonflyController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class DragonflyController : MonoBehaviour
+{
+    public float jumpForce = 12f;
+    private Rigidbody2D rb;
+    private Vector2 originalPosition; // Store the original position
+    private Renderer rend; // Reference to the character's Renderer
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        originalPosition = transform.position; // Save the original position
+        rend = GetComponent<Renderer>(); // Get the Renderer component
+    }
+
+    private void Update()
+    {
+        // Check if the screen has been touched or clicked
+        if (Input.GetMouseButtonDown(0) || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        // Convert world coordinates to viewport coordinates
+        Vector2 viewportPositionMin = Camera.main.WorldToViewportPoint(rend.bounds.min);
+        Vector2 viewportPositionMax = Camera.main.WorldToViewportPoint(rend.bounds.max);
+
+        // Check if the character is completely outside the screen bounds
+        if (viewportPositionMax.y < 0 || viewportPositionMin.y > 1)
+        {
+            // Reset to the original position
+            transform.position = originalPosition;
+            rb.velocity = Vector2.zero; // Reset velocity
+        }
+    }
+}
