d297d67a6ca32500f5626b5b596e645b7bd259d9
JeDa187 <j.saarela@dr.com>
2023-08-15 12:26:54 +0300

    Score-järjestelmän kehittäminen

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -7,13 +7,20 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject gameOverPanel;           // Panel displayed when the game is over
     [SerializeField] private TMP_Text countdownTextObject;       // Text object for the countdown
+    [SerializeField] private TMP_Text scoreTextObject;           // Text object for the score
+    [SerializeField] private TMP_Text highScoreTextObject;       // Text object for the high score
+
+    private const string HighScoreKey = "HighScore";             // Key for PlayerPrefs
 
     private float countdownTime = 3.0f;                          // Time for the countdown before the game starts
+    private int score = 0;                                       // Player's score
+    private int highScore = 0;                                   // High score
     public static GameManager Instance;                          // Singleton instance of the GameManager
 
     private void Awake()
     {
         SetupSingleton();                                        // Setup the Singleton instance
+        LoadHighScore();                                         // Load the high score from PlayerPrefs
     }
 
     private void Start()
@@ -42,13 +49,32 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         gameOverPanel.SetActive(true);                           // Display the Game Over panel
+        scoreTextObject.gameObject.SetActive(false);             // Hide the score text
         Time.timeScale = 0;                                      // Pause the game time
+
+        UpdateHighScore();                                       // Update the high score if necessary
     }
 
     public void RestartGame()
     {
         Time.timeScale = 1;                                      // Resume game time
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);  // Reload the current scene
+    }
+
+    private void LoadHighScore()
+    {
+        highScore = PlayerPrefs.GetInt(HighScoreKey, 0);         // Load the high score from PlayerPrefs
+        highScoreTextObject.text = $"High Score: {highScore}";   // Display the high score
+    }
+
+    private void UpdateHighScore()
+    {
+        if (score > highScore)                                   // Check if the score is greater than the high score
+        {
+            highScore = score;                                   // Update the high score
+            PlayerPrefs.SetInt(HighScoreKey, highScore);         // Save the high score to PlayerPrefs
+        }
+        highScoreTextObject.text = $"High Score: {highScore}";   // Display the high score
     }
 
     private IEnumerator CountdownToStart()
@@ -68,10 +94,27 @@ public class GameManager : MonoBehaviour
 
         countdownTextObject.text = "";                          // Clear countdown text
 
+        score = 0;                                             // Reset the score
+        scoreTextObject.text = $"Score: {score}";               // Set the score text to "Score: 0"
+        scoreTextObject.gameObject.SetActive(true);             // Show the score text
+
         if (dragonfly != null)
         {
             dragonfly.ResetDragonfly();                         // Reset the dragonfly position and state
             dragonfly.ToggleRigidbodyMovement(true);            // Enable dragonfly movement
+        }
+
+        StartCoroutine(UpdateScore());
+    }
+
+
+    private IEnumerator UpdateScore()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);              // Wait for 0.5 seconds
+            score += 1;                                         // Increase the score by 1
+            scoreTextObject.text = $"Score: {score}";           // Update the score text
         }
     }
 }
