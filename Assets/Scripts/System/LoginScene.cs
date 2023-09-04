612f777888bc1a981ed67f06115e2172d2d0f58c
JeDa187 <j.saarela@dr.com>
2023-09-04 13:59:53 +0300

    Kirjautuneen käyttäjän näyttäminen

--- Assets/Scripts/System/LoginScene.cs
+++ Assets/Scripts/System/LoginScene.cs
@@ -26,6 +26,7 @@ public class LoginScene : MonoBehaviour
         }
         else
         {
+            PlayerPrefs.SetString("PlayerName", playerName); // Tallenna kirjautuneen käyttäjän nimi.
             CheckPlayerName(playerName, password);
         }
     }
--- Assets/Scripts/System/MainMenu.cs
+++ Assets/Scripts/System/MainMenu.cs
@@ -1,8 +1,26 @@
-using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    public TextMeshProUGUI playerStatusText; // Viittaus uuteen UI-elementtiin.
+
+    private void Start()
+    {
+        // P‰ivit‰ teksti riippuen siit‰, onko k‰ytt‰j‰ kirjautunut sis‰‰n vai ei.
+        if (PlayerPrefs.GetInt("Online") == 1)
+        {
+            //Kirjautuneen k‰ytt‰j‰n nimi on tallennettu PlayerPrefsiin.
+            string playerName = PlayerPrefs.GetString("PlayerName", "Unknown");
+            playerStatusText.text = playerName;
+        }
+        else
+        {
+            playerStatusText.text = "Offline Mode";
+        }
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("GameScene");
