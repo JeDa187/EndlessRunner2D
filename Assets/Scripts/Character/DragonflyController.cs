547e49fdd64f132ffbbe7d383b79e99c8243000d
milquejas <84683894+milquejas@users.noreply.github.com>
2023-09-11 10:28:43 +0300

    päivitetty ja järjestelty character controlleria

--- Assets/Scripts/Character/DragonflyController.cs
+++ Assets/Scripts/Character/DragonflyController.cs
@@ -8,7 +8,8 @@ public class DragonflyController : MonoBehaviour /*IAbilityActivator*/
 
     private Vector2 originalPosition; // Original position of the dragonfly for resetting  
     [SerializeField] private float jumpForce;
-    private bool canMove = false; 
+    private bool canMove = false;
+    private InfiniteParallaxBackground parallaxBG;
     private Rigidbody2D rb;
 
     private float immortalTimer = 0;
@@ -17,13 +18,14 @@ public class DragonflyController : MonoBehaviour /*IAbilityActivator*/
     private AbilitySO abilitySO;
     private bool isMultiplierActive = false;
     private RigidbodyConstraints2D originalConstraints;
-
     private float[] originalSpeeds;
 
 
     private void Awake()
     {
+        parallaxBG = FindObjectOfType<InfiniteParallaxBackground>();
         rb = GetComponent<Rigidbody2D>(); // Initialize the Rigidbody2D component here instead of Start
+        originalConstraints = rb.constraints;
     }
 
     private void Start()
@@ -38,7 +40,6 @@ public class DragonflyController : MonoBehaviour /*IAbilityActivator*/
         if (!canMove) return; // If canMove is false, exit the function
         inputHandling.HandleInput();        // Check and process player input
     }
-
 
     public void Jump()
     {
@@ -79,6 +80,7 @@ public class DragonflyController : MonoBehaviour /*IAbilityActivator*/
             StartCoroutine(FireBreathCoroutine());
         }
     }
+
     private IEnumerator FireBreathCoroutine()
     {
         Debug.Log("coroutine");
@@ -116,15 +118,17 @@ public class DragonflyController : MonoBehaviour /*IAbilityActivator*/
         // Stop Fire Breath Particles
         HandleFireBreathParticles(false);
     }
+
     private void SaveOriginalParallaxSpeeds()
     {
-        InfiniteParallaxBackground parallaxBG = FindObjectOfType<InfiniteParallaxBackground>();
         originalSpeeds = (float[])parallaxBG.LayerScrollSpeeds.Clone();
     }
+
     private void ResetParallaxSpeed()
     {
         ManipulateParallaxSpeed(false);
     }
+
     private void ManipulateParallaxSpeed(bool increase)
     {
         InfiniteParallaxBackground parallaxBG = FindObjectOfType<InfiniteParallaxBackground>();
@@ -143,21 +147,10 @@ public class DragonflyController : MonoBehaviour /*IAbilityActivator*/
             }
         }
     }
-    //private void ManipulateParallaxSpeed(bool increase)
-    //{
-    //    InfiniteParallaxBackground parallaxBG = FindObjectOfType<InfiniteParallaxBackground>();
-    //    float multiplier = increase ? 20f : 1f;
-
-    //    for (int i = 0; i < parallaxBG.LayerScrollSpeeds.Length; i++)
-    //    {
-    //        parallaxBG.LayerScrollSpeeds[i] *= multiplier;
-    //    }
-    //}
 
     private void ActivateImmortality()
     {
         immortalTimer = abilitySO.abilityDuration;
-        RigidbodyConstraints2D originalConstraints = rb.constraints;
         rb.constraints = RigidbodyConstraints2D.FreezePositionY;
         GetComponent<Collider2D>().enabled = false;
     }
@@ -190,52 +183,6 @@ public class DragonflyController : MonoBehaviour /*IAbilityActivator*/
         }
     }
 
-    //private IEnumerator FireBreathCoroutine()
-    //{
-    //    Debug.Log("coroutine");
-    //    InfiniteParallaxBackground parallaxBG = FindObjectOfType<InfiniteParallaxBackground>();
-    //    float[] originalSpeeds = (float[])parallaxBG.LayerScrollSpeeds.Clone(); // Save the original speeds
-    //    for (int i = 0; i < parallaxBG.LayerScrollSpeeds.Length; i++)
-    //    {
-    //        parallaxBG.LayerScrollSpeeds[i] *= 20f;
-    //    }
-
-    //    immortalTimer = abilitySO.abilityDuration;
-    //    RigidbodyConstraints2D originalConstraints = rb.constraints;
-    //    rb.constraints = RigidbodyConstraints2D.FreezePositionY;
-    //    GetComponent<Collider2D>().enabled = false;
-
-    //    Debug.Log("Checking fireBreathParticles");
-    //    if (abilitySO.fireBreathParticles != null)
-    //    {
-    //        Debug.Log("Fire breath particles found");
-    //        abilitySO.fireBreathParticles.Play();
-    //    }
-    //    else
-    //    {
-    //        Debug.Log("No fire breath particles found");
-    //    }
-
-    //    isMultiplierActive = true; // The score multiplier is now active
-
-    //    yield return new WaitForSeconds(abilitySO.abilityDuration);
-
-    //    isMultiplierActive = false;
-
-    //    // Reset the speeds back to the original values
-    //    for (int i = 0; i < parallaxBG.LayerScrollSpeeds.Length; i++)
-    //    {
-    //        parallaxBG.LayerScrollSpeeds[i] = originalSpeeds[i];
-    //    }
-    //    immortalTimer = 0;
-    //    rb.constraints = originalConstraints;
-    //    GetComponent<Collider2D>().enabled = true;
-
-    //    if (abilitySO.fireBreathParticles != null)
-    //    {
-    //        abilitySO.fireBreathParticles.Stop();
-    //    }
-    //}
     public int GetScoreMultiplier()
     {
         return isMultiplierActive ? 2 : 1; // Return 2 if multiplier is active, otherwise return 1
