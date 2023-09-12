82579553335aa722c070b730106ba6227e6a8d8f
JeDa187 <j.saarela@dr.com>
2023-09-12 13:28:35 +0300

    update

--- Assets/Scripts/System/CharacterSelection.cs
+++ Assets/Scripts/System/CharacterSelection.cs
@@ -9,30 +9,34 @@ using System.Collections;
 
 public class CharacterSelection : MonoBehaviour
 {
+    // Singleton Instance
     public static CharacterSelection Instance { get; private set; }
 
+    // Character Details
+    [Header("Character Details")]
     public Sprite[] characterSprites; // Array of character sprites
-    public int selectedCharacterIndex = 0; // Index of currently selected character
+    public int selectedCharacterIndex = -1; // Index of currently selected character
     public bool[] characterLocked = { false, true, true }; // Array indicating if a character is locked
 
+    // UI Elements
+    [Header("UI Elements")]
     public Button[] characterButtons; // Array of character selection buttons
+    public Image[] characterImages; // Images representing the characters
+    public TMP_Text[] characterUnlockTexts; // Array of text fields for character unlock status
+    public TMP_Text infoText; // Text field for displaying information to the player
+
+    // Visual Appearance
+    [Header("Visual Appearance")]
     public Color equippedColor = Color.gray; // Color to indicate a character is equipped
-
-    public LoadingPanelManager loadingPanelManager; // Referenssi LoadingPanelManager-skriptiin
-
     public Color[] buttonColors; // Array to store default colors for each button
 
-    public TMP_Text[] characterUnlockTexts; // Array of text fields for character unlock status
-
-    public TMP_Text infoText; // Text field for displaying information to the player
-    public Image[] characterImages;
-
-
-
-
+    // Loading Panel
+    [Header("Loading Panel")]
+    public LoadingPanelManager loadingPanelManager; // Reference to LoadingPanelManager script
 
     private void Awake()
     {
+        selectedCharacterIndex = -1;
         // Singleton pattern to ensure only one instance of CharacterSelection exists
         if (Instance == null || Instance.gameObject.scene != this.gameObject.scene)
         {
@@ -50,56 +54,68 @@ public class CharacterSelection : MonoBehaviour
 
     private void Start()
     {
-        // Check if the game is in offline mode
+        selectedCharacterIndex = -1;
+        // Tarkista, onko peli offline-tilassa
         if (PlayerPrefs.GetInt("Online", 1) == 0)
         {
-            // Lock all characters except the first one
             characterLocked[0] = false;
             characterLocked[1] = true;
             characterLocked[2] = true;
             UpdateCharacterTexts();
             UpdateButtonColors();
             loadingPanelManager.HideLoadingPanel();
-
-            // Update info text
-            infoText.text = "Additional characters are unavailable in offline mode, Press 'Continue' to play."; // Ei muutosta tähän riviin
+            infoText.text = "Additional characters are unavailable in offline mode. You can only select the default character.";
         }
         else
         {
-            loadingPanelManager.ShowLoadingPanel(); // Kutsu LoadingPanelManagerin metodia
-            FetchPlayerHighScore(); // Fetch the player's high score
+            loadingPanelManager.ShowLoadingPanel();
+            FetchPlayerHighScore();
 
-            // Added this block to inform the player which character is equipped by default
+            // If a valid character is not selected, set the message to ask the player to select one
+            if (selectedCharacterIndex < 0 || selectedCharacterIndex >= characterSprites.Length)
+            {
+                infoText.text = "Select your character.";
+                return; // Stop further execution
+            }
+
+            // Update the info text based on the selected character
+            string additionalMessage = AdditionalCharactersAvailableMessage();
             switch (selectedCharacterIndex)
             {
                 case 0:
-                    infoText.text = "Default character equipped. Press 'Continue' to play.";
+                    infoText.text = "Default character equipped. Press Continue to play. " + additionalMessage;
                     break;
                 case 1:
-                    infoText.text = "Character 2 equipped. Press 'Continue' to play.";
+                    infoText.text = "Character 2 equipped. Press Continue to play. " + additionalMessage;
                     break;
                 case 2:
-                    infoText.text = "Character 3 equipped. Press 'Continue' to play.";
-                    break;
-                default:
-                    infoText.text = "Select a character to play.";
+                    infoText.text = "Character 3 equipped. Press Continue to play. " + additionalMessage;
                     break;
             }
         }
     }
 
 
-
+    // Switch to the Main Menu scene
     public void GoToMainMenu()
     {
+        if (selectedCharacterIndex < 0 || selectedCharacterIndex > 2)
+        {
+            Debug.LogWarning("Invalid character index. Cannot go to the main menu.");
+
+            // Näytä tilapäinen viesti pelaajalle.
+            StartCoroutine(DisplayTemporaryMessage("You must choose a character to continue.", 2f));
+            return;
+        }
+
         SceneManager.LoadScene("MainMenu"); // Load the main menu scene
     }
 
+    // Equip a character based on the index provided
     public void EquipCharacter(int index)
     {
         bool isOnline = PlayerPrefs.GetInt("Online", 1) == 1;
 
-        // Equip the character if it's not locked
         if (index < characterSprites.Length && !characterLocked[index])
         {
             selectedCharacterIndex = index;
@@ -107,36 +123,55 @@ public class CharacterSelection : MonoBehaviour
             UpdateButtonColors();
             UpdateCharacterTexts();
 
-            // This part updates the infoText based on the selected character
+            string additionalMessage = AdditionalCharactersAvailableMessage();
             switch (index)
             {
                 case 0:
-                    infoText.text = "Default character equipped. Press 'Continue' to play.";
+                    infoText.text = "Default character equipped. Press Continue to play. " + additionalMessage;
                     break;
                 case 1:
-                    infoText.text = "Character 2 equipped. Press 'Continue' to play.";
+                    infoText.text = "Character 2 equipped. Press Continue to play. " + additionalMessage;
                     break;
                 case 2:
-                    infoText.text = "Character 3 equipped. Press 'Continue' to play.";
+                    infoText.text = "Character 3 equipped. Press Continue to play. " + additionalMessage;
                     break;
+
             }
         }
         else if (characterLocked[index] && isOnline)
         {
             Debug.Log($"Character with index {index} is locked.");
-            StartCoroutine(DisplayTemporaryMessage("This character is still waiting in the shadows, Achieve a higher score to unlock.", 3f)); // display for 3 seconds.
-
+            StartCoroutine(DisplayTemporaryMessage("This character is still waiting in the shadows, achieve a higher score to unlock.", 2f));
         }
         else if (characterLocked[index] && !isOnline)
         {
             Debug.Log($"Character with index {index} is locked.");
-            StartCoroutine(DisplayTemporaryMessage("This character is unavailable in offline mode.", 3f));
+            StartCoroutine(DisplayTemporaryMessage("This character is unavailable in offline mode.", 2f));
         }
         else
         {
             Debug.Log($"Invalid index {index}. Character not equipped.");
         }
     }
+
+    // Message to display additional available characters
+    private string AdditionalCharactersAvailableMessage()
+    {
+        int unlockedCharacters = 0;
+        for (int i = 0; i < characterLocked.Length; i++)
+        {
+            if (!characterLocked[i] && i != selectedCharacterIndex)
+                unlockedCharacters++;
+        }
+
+        if (unlockedCharacters > 0)
+        {
+            return "Or select from the available characters.";
+        }
+        return "";
+    }
+
+    // Display a message for a temporary duration
     private IEnumerator DisplayTemporaryMessage(string message, float duration)
     {
         string previousMessage = infoText.text; // Store the current message
@@ -146,10 +181,9 @@ public class CharacterSelection : MonoBehaviour
     }
 
 
-
+    // Fetch player's high score from the PlayFab service
     void FetchPlayerHighScore()
     {
-        // Fetch the player's high score from PlayFab
         PlayFabClientAPI.GetPlayerStatistics(new GetPlayerStatisticsRequest(),
         result => {
             bool scoreFound = false;
@@ -168,7 +202,7 @@ public class CharacterSelection : MonoBehaviour
             {
                 PlayerPrefs.SetInt("PlayerScore", 0);
             }
-
+            // Update character lock status and texts after fetching the score
             UpdateCharacterLocks();
             UpdateCharacterTexts();
             loadingPanelManager.HideLoadingPanel(); // Hide the loading panel once data is fetched
@@ -179,6 +213,7 @@ public class CharacterSelection : MonoBehaviour
         });
     }
 
+    // Lock or unlock characters based on the player's score
     void UpdateCharacterLocks()
     {
         // Update which characters are locked based on the player's score
@@ -199,33 +234,34 @@ public class CharacterSelection : MonoBehaviour
         UpdateButtonColors();
     }
 
+    // Update button colors based on character availability and selection
     void UpdateButtonColors()
     {
-        // Update the color of the character buttons and their images
         for (int i = 0; i < characterButtons.Length; i++)
         {
             if (i == selectedCharacterIndex)
             {
                 characterButtons[i].image.color = equippedColor;
-                characterImages[i].color = Color.white; // restore the image's original color
+                characterImages[i].color = Color.white;
             }
             else if (characterLocked[i]) // Check if character is locked
             {
                 characterImages[i].color = Color.black; // Set the character image color to black for the silhouette
             }
-            else if (i < buttonColors.Length) // Ensure we don't go out of bounds
+            else if (i < buttonColors.Length)
             {
-                characterImages[i].color = Color.white; // restore the image's original color
+                characterImages[i].color = Color.white;
                 characterButtons[i].image.color = buttonColors[i];
             }
             else
             {
-                characterImages[i].color = Color.white; // restore the image's original color
+                characterImages[i].color = Color.white;
                 characterButtons[i].image.color = Color.white;
             }
         }
     }
 
+    // Update text displays on character buttons
     void UpdateCharacterTexts()
     {
         bool isOnline = PlayerPrefs.GetInt("Online", 1) == 1;
@@ -239,9 +275,10 @@ public class CharacterSelection : MonoBehaviour
                 {
                     buttonText.text = "Equipped";
                 }
-                else if (!characterLocked[i] && isOnline)
+                else if (!characterLocked[i])
                 {
-                    buttonText.text = "Equip";
+                    // Jos olemme offline ja hahmo on indexissä 0, näytetään "Equip"
+                    buttonText.text = (!isOnline && i == 0) ? "Equip" : (isOnline ? "Equip" : "Locked");
                 }
                 else
                 {
@@ -268,4 +305,5 @@ public class CharacterSelection : MonoBehaviour
             characterUnlockTexts[2].text = "Not available in offline mode";
         }
     }
+
 }
